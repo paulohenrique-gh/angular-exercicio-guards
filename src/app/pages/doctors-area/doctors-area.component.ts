75ac8d5b2ba3e967e45ea6985f6af68a87f3458e
Paulo Henrique <ph.1928@hotmail.com>
2024-09-04 22:11:59 -0300

    feat: add doctors area component

---
+++ src/app/pages/doctors-area/doctors-area.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-doctors-area',
+  standalone: true,
+  imports: [],
+  templateUrl: './doctors-area.component.html',
+  styleUrl: './doctors-area.component.css'
+})
+export class DoctorsAreaComponent {
+
+}
