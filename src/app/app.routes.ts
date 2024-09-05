0b21c0874e5fd4dd1eaebbf795dcec9af37f4802
Paulo Henrique <ph.1928@hotmail.com>
2024-09-04 22:17:13 -0300

    feat: add routes for login, admin-area, doctors-area

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { AdminAreaComponent } from './pages/admin-area/admin-area.component';
+import { DoctorsAreaComponent } from './pages/doctors-area/doctors-area.component';
+import { authGuard } from './guards/auth.guard';
+import { doctorGuard } from './guards/doctor.guard';
+import { adminGuard } from './guards/admin.guard';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'admin-area',
+        component: AdminAreaComponent,
+        canActivate: [authGuard, adminGuard]
+    },
+    {
+        path: 'doctors-area',
+        component: DoctorsAreaComponent,
+        canActivate: [authGuard, doctorGuard]
+    }
+];
