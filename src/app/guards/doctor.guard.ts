352e3292b0e30e548ee7c92bd1a3c49e3f452ffe
Paulo Henrique <ph.1928@hotmail.com>
2024-09-04 22:16:11 -0300

    feat: add doctor guard for doctor-area route

---
+++ src/app/guards/doctor.guard.ts
@@ -0,0 +1,25 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { DecodedTokenWithRoles } from '../models/decoded-token-with-roles';
+import { jwtDecode } from 'jwt-decode';
+
+export const doctorGuard: CanActivateFn = (route, state) => {
+  const router = inject(Router);
+
+  const token = localStorage.getItem('token');
+  let decodedToken: DecodedTokenWithRoles | undefined;
+  let roles: string[] | undefined;
+  if (token !== null) {
+    decodedToken = jwtDecode(token);
+    roles = decodedToken?.roles?.split(',');
+  }
+
+  console.log(roles)
+
+  if (roles && roles.includes('ROLE_DOCTOR')) {
+    return true;
+  } else {
+    alert('Unauthorized');
+    return false;
+  }
+};
