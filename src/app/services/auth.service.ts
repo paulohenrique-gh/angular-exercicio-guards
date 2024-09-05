e30b24c4d3c1776db5fbbe473b9fb47ed62401f6
Paulo Henrique <ph.1928@hotmail.com>
2024-09-04 22:14:53 -0300

    feat: add AuthService for user login and logout

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { User } from '../models/user';
+import { catchError, Observable, tap, throwError } from 'rxjs';
+import { AuthToken } from '../models/auth-token';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private url = 'http://localhost:8080/auth';
+
+  constructor(private httpClient: HttpClient) {}
+
+  login(user: User): Observable<AuthToken> {
+    return this.httpClient.post<AuthToken>(`${this.url}/login`, user).pipe(
+      tap((res: AuthToken) => {
+        localStorage.setItem('token', res.token);
+        return res;
+      }),
+      catchError((error) => {
+        return throwError(() => error);
+      })
+    );
+  }
+
+  logout(): Observable<any> {
+    return this.httpClient.post(`${this.url}/logout`, {}).pipe(
+      tap((res) => {
+        console.log(res)
+        localStorage.removeItem('token')
+      }),
+      catchError((error) => {
+        return throwError(() => error);
+      })
+    );
+  }
+}
