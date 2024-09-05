41610b5628d770bf7c720e06bbe077bc1e04812c
Paulo Henrique <ph.1928@hotmail.com>
2024-09-04 22:11:51 -0300

    feat: add admin area component

---
+++ src/app/pages/admin-area/admin-area.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-area',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-area.component.html',
+  styleUrl: './admin-area.component.css'
+})
+export class AdminAreaComponent {
+
+}
