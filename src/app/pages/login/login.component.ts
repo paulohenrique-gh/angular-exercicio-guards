b446f350532a8c823ac9c634e429379a4a1051e5
Paulo Henrique <ph.1928@hotmail.com>
2024-09-04 22:11:40 -0300

    feat: add login component

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { User } from '../../models/user';
+import { AuthService } from '../../services/auth.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+})
+export class LoginComponent {
+  user: User = {
+    username: '',
+    password: '',
+  };
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  onSubmit(): void {
+    this.authService.login(this.user).subscribe({
+      next: () => {
+        const route = this.activatedRoute.snapshot.queryParamMap.get('redirectTo') || '';
+        this.router.navigateByUrl(route);
+      },
+      error: (err) => alert(err)
+    });
+  }
+}
