0aba7511e680e8d428abfbe2e2c04fdde194b2f6
Paulo Henrique <ph.1928@hotmail.com>
2024-09-05 12:03:51 -0300

    feat: add auth guard to all routes except login
    add routes for about and contact pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,25 +6,40 @@ import { authGuard } from './guards/auth.guard';
 import { doctorGuard } from './guards/doctor.guard';
 import { adminGuard } from './guards/admin.guard';
 import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 export const routes: Routes = [
-    {
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: '',
+    canActivateChild: [authGuard],
+    children: [
+      {
         path: '',
-        component: HomeComponent,
-        canActivate: [authGuard]
-    },
-    {
-        path: 'login',
-        component: LoginComponent
-    },
-    {
+        component: HomeComponent
+      },
+      {
+        path: 'about',
+        component: AboutComponent
+      },
+      {
+        path: 'contact',
+        component: ContactComponent
+      },
+      {
         path: 'admin-area',
         component: AdminAreaComponent,
-        canActivate: [authGuard, adminGuard]
-    },
-    {
+        canActivate: [adminGuard],
+      },
+      {
         path: 'doctors-area',
         component: DoctorsAreaComponent,
-        canActivate: [authGuard, doctorGuard]
-    }
+        canActivate: [doctorGuard],
+      },
+    ],
+  },
 ];
