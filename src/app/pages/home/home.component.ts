c469eaf5b59d54846b0812576538bd4b6cd9ae86
Paulo Henrique <ph.1928@hotmail.com>
2024-09-04 22:12:20 -0300

    feat: add home component

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+})
+export class HomeComponent {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onLogout(): void {
+    this.authService.logout().subscribe({
+      next: () => this.router.navigateByUrl('login'),
+      error: (err) => console.log(err),
+    });
+  }
+}
