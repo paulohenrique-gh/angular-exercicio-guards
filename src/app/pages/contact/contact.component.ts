bcaf6a9a2a4935bfbc34cb03e474352f6e90f7c4
Paulo Henrique <ph.1928@hotmail.com>
2024-09-06 13:51:01 -0300

    feat: set boolean flag to true when user submits email

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  email: string = '';
+  isAllowedToLeave: boolean = false;
 
+  onSubmit(): void {
+    this.isAllowedToLeave = true;
+  }
 }
