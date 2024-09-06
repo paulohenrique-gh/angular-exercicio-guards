a54c087ee004c0854ba943b6571f8fc20f37aebb
Paulo Henrique <ph.1928@hotmail.com>
2024-09-06 13:52:51 -0300

    refactor: change AuthService to return an obsevable for other components to subscribe to and be notified when user logs out

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from '../models/user';
-import { catchError, Observable, tap, throwError } from 'rxjs';
+import { BehaviorSubject, catchError, Observable, tap, throwError } from 'rxjs';
 import { AuthToken } from '../models/auth-token';
 
 @Injectable({
@@ -9,18 +9,35 @@ import { AuthToken } from '../models/auth-token';
 })
 export class AuthService {
   private url = 'http://localhost:8080/auth';
+  private loggedInSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient) {
+    if (localStorage.getItem("token") !== null) {
+      this.loggedInSubject.next(true);
+    } else {
+      this.loggedInSubject.next(false);
+    }
+  }
 
   login(user: User): Observable<AuthToken> {
     return this.httpClient.post<AuthToken>(`${this.url}/login`, user).pipe(
       tap((res: AuthToken) => {
         localStorage.setItem('token', res.token);
+        this.loggedInSubject.next(true);
         return res;
       }),
       catchError((error) => {
         return throwError(() => error);
       })
     );
+  }
+
+  logout(): void {
+    localStorage.removeItem("token");
+    this.loggedInSubject.next(false);
+  }
+
+  isLoggedIn(): Observable<boolean> {
+    return this.loggedInSubject.asObservable();
   }
 }
